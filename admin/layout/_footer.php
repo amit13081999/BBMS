badf9088d874c3eddcf47c27e4fd31475acbf0d2
Amit <amitraghuvanshi13081999@gmail.com>
2022-03-30 00:38:03 +0530

    initial commit

---
+++ admin/layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="nav navbar-fixed-bottom">
+            <b>Made By: Amit Raghuvanshi</b>
+            <br>
+            &COPY;SCSIT
+</footer>
+
+
+
+</body>
+</html>
---
+++ layout/_footer.php
@@ -0,0 +1,12 @@
+<footer class="foter">
+            <b>Made By: Amit Raghuvanshi</b>
+            <br>
+            &COPY;SCSIT
+            <br>
+            Contact us : 9826429472
+</footer>
+
+
+
+</body>
+</html>
